66feba52023e06d611199b4c62ccf7a12547f6b5
wxk38bdc <2227814847@qq.com>
2023-12-07 07:55:24 +0800

    daily submission 2023/12/7

--- Project_2023_12_1/bite_new.c
+++ Project_2023_12_1/bite_new.c
@@ -25,38 +25,47 @@
 //	return 0;
 //}
 
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//int main()
+//{
+//	int* p = (int*)calloc(10,sizeof(int));//calloc初始化为0
+//	if (p == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//		return 1;
+//	}
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", p[i]);
+//	}
+//	//空间不够，想要扩容
+//	int* ptr = (int*)realloc(p, 20*sizeof(int));//不能用p接收，要用新的指针
+//	//realloc会先把原来的空间拷贝到新的空间，再释放原来的空间
+//	//如果新的空间分配失败，原来的空间还在，所以要用新的指针接收
+//	//如果新的空间分配成功，原来的空间就释放了，所以要用新的指针接收
+//	if (ptr == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//		return 1;
+//	}
+//	else
+//	{
+//		p = ptr;
+//	}
+//
+//	free(p);
+//	p = NULL;//防止野指针
+//	return 0;
+//}
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 int main()
 {
-	int* p = (int*)calloc(10,sizeof(int));//calloc初始化为0
-	if (p == NULL)
-	{
-		printf("%s\n", strerror(errno));
-		return 1;
-	}
-	int i = 0;
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d ", p[i]);
-	}
-	//空间不够，想要扩容
-	int* ptr = (int*)realloc(p, 20*sizeof(int));//不能用p接收，要用新的指针
-	//realloc会先把原来的空间拷贝到新的空间，再释放原来的空间
-	//如果新的空间分配失败，原来的空间还在，所以要用新的指针接收
-	//如果新的空间分配成功，原来的空间就释放了，所以要用新的指针接收
-	if (ptr == NULL)
-	{
-		printf("%s\n", strerror(errno));
-		return 1;
-	}
-	else
-	{
-		p = ptr;
-	}
-
-	free(p);
-	p = NULL;//防止野指针
+	int*p=realloc(NULL, 20 * sizeof(int));//realloc(NULL,20)等价于malloc(20)
 	return 0;
 }
