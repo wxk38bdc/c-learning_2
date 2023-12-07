26b3250d1342bdb98677087ecff24e09170b0548
wxk38bdc <2227814847@qq.com>
2023-12-08 07:49:25 +0800

    daily submission 2023/12/8

---
+++ Project_2023_12_1/NKU_new.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<iostream>
+//using namespace std;
+//#include<cstdio>
+//int main()
+//{
+//	int a = 5;
+//	int b = 10;
+//	int* pa = &a;
+//	int* pb = &b;
+//	printf("a=%d b=%d *pa=%d *pb=%d\n", a, b, *pa, *pb);
+//	a = 1;
+//	b = 2;
+//	printf("a=%d b=%d *pa=%d *pb=%d\n", a, b, *pa, *pb);
+//	return 0;
+//}
--- Project_2023_12_1/bite_new.c
+++ Project_2023_12_1/bite_new.c
@@ -54,6 +54,7 @@
 //	else
 //	{
 //		p = ptr;
+//		printf("\n扩容成功\n");
 //	}
 //
 //	free(p);
@@ -61,11 +62,163 @@
 //	return 0;
 //}
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-int main()
-{
-	int*p=realloc(NULL, 20 * sizeof(int));//realloc(NULL,20)等价于malloc(20)
-	return 0;
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//int main()
+//{
+//	//int*p=realloc(NULL, 20 * sizeof(int));//realloc(NULL,20)等价于malloc(20)
+//	int* p = (int*)malloc(10 * sizeof(int));
+//	if (p == NULL)
+//	{
+//		return 1;
+//	}
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		*(p + i) = i;
+//	}
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//void GetMemory(char** p)
+//{
+//	*p = (char*)malloc(100);
+//}
+//void Test(void)
+//{
+//	char* str = NULL;
+//	GetMemory(&str);
+//	strcpy(str, "hello world");
+//	printf(str);
+//	free(str);
+//	str = NULL;
+//}
+//int main()
+//{
+//	Test();
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//char* GetMemory(void)
+//{
+//	static char p[] = "hello world!";//加static存储在常量区，出作用域不销毁
+//	return p;
+//}
+//void Test(void)
+//{
+//	char* str = NULL;
+//	str = GetMemory();
+//	printf(str);
+//
+//	str = NULL;
+//}
+//int main()
+//{
+//	Test();
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//int* test(int** p)
+//{
+//	**p = 10086;
+//	return *p;
+//}
+//int main()
+//{
+//	int n = 0;
+//	int* tmp = &n;
+//	tmp = test(&tmp);
+//	printf("hello\n");
+//	printf("%d", *tmp);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include<stdlib.h>
+//#include<string.h>
+//typedef struct stu {
+//	char name[20];
+//	int age;
+//	int a[];//柔性数组成员
+//}stu;
+//int main()
+//{
+//	stu* ps = (stu*)malloc(sizeof(stu) + 10 * sizeof(int));
+//	strcpy(ps->name , "zhangsan");
+//	
+//	ps->age = 20;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		ps->a[i] = 2 * i + 1;
+//	}
+//	printf("%s\n",ps->name);
+//	printf("%d\n", ps->age);
+//	for(i=0;i<10;i++)
+//		printf("%d ", ps->a[i]);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main()
+//{
+//    int** arr = (int**)malloc(3 * sizeof(int*)); // 为行分配内存
+//    for (int i = 0; i < 3; i++)
+//    {
+//        arr[i] = (int*)malloc(5 * sizeof(int)); // 为列分配内存
+//    }
+//    int num = 1;
+//    for (int i = 0; i < 3; i++)
+//    {
+//        for (int j = 0; j < 5; j++)
+//        {
+//            arr[i][j] = num;
+//            num++;
+//        }
+//    }
+//    for (int i = 0; i < 3; i++)
+//    {
+//        for (int j = 0; j < 5; j++)
+//        {
+//            printf("%2d ", arr[i][j]);
+//        }
+//        printf("\n");
+//    }
+//    // 释放分配的内存
+//    for (int i = 0; i < 3; i++)
+//    {
+//        free(arr[i]);
+//    }
+//    free(arr);
+//
+//    return 0;
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//int* getConcatenation(int* nums, int numsSize, int* returnSize) {
+//	int* ans = realloc(NULL, 2 * numsSize * sizeof(int));
+//	int i = 0;
+//	for (i = 0; i < numsSize; i++)
+//	{
+//		ans[i] = nums[i];
+//	}
+//	for (i = numsSize; i < numsSize * 2; i++)
+//	{
+//		ans[i] = nums[i - numsSize];
+//	}
+//	return ans;
+//}
