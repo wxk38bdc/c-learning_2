1d4eba7c0d7c9f57fe83da2cdf307fe3fc9383d2
wxk38bdc <2227814847@qq.com>
2024-01-03 07:55:35 +0800

    daily submission 2024/1/3

--- BinaryTree/test.c
+++ BinaryTree/test.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<assert.h>
+#include<stdlib.h>
 
 typedef char BTNDataType;
 typedef struct BinaryTreeNode
@@ -10,14 +11,88 @@ typedef struct BinaryTreeNode
 	BTNDataType _data;
 }BTNode;
 
-int TreeSize(BTNode* root)
+BTNode* CreateNode(BTNDataType x)//创建节点
+{
+	BTNode* node = (BTNode*)malloc(sizeof(BTNode));
+	node->_data = x;
+	node->_left = NULL;
+	node->_right = NULL;
+	return node;
+}
+int TreeSize(BTNode* root)//求二叉树的节点个数
 {
 	if (root == NULL)
 		return 0;
 	return 1 + TreeSize(root->_left) + TreeSize(root->_right);
 }
+int TreeLeafSize(BTNode* root)//求二叉树的叶子节点个数
+{
+	if (root == NULL)
+		return 0;
+	if (root->_left == NULL&&root->_right == NULL)
+		return 1;
+	return TreeLeafSize(root->_left) + TreeLeafSize(root->_right);
+}
+int TreeDepth(BTNode* root)//求二叉树的深度
+{
+	if (root == NULL)
+		return 0;
+	int leftDepth = TreeDepth(root->_left);
+	int rightDepth = TreeDepth(root->_right);
+	return leftDepth > rightDepth ? leftDepth + 1 : rightDepth + 1;
+}
+void PreOrder(BTNode* root)//先序遍历
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+	printf("%c ", root->_data);
+	PreOrder(root->_left);
+	PreOrder(root->_right);
+}
+void InOrder(BTNode* root)//中序遍历
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+	InOrder(root->_left);
+	printf("%c ", root->_data);
+	InOrder(root->_right);
+}
+void PostOrder(BTNode* root)//后序遍历
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+	PostOrder(root->_left);
+	PostOrder(root->_right);
+	printf("%c ", root->_data);
+}
 int main()
 {
-
+	BTNode* A = CreateNode('A');
+	BTNode* B = CreateNode('B');
+	BTNode* C = CreateNode('C');
+	BTNode* D = CreateNode('D');
+	BTNode* E = CreateNode('E');
+	A->_left = B;
+	A->_right = C;
+	B->_left = D;
+	B->_right = E;
+	printf("TreeSize=%d\n", TreeSize(A));
+	PreOrder(A);
+	printf("\n");
+	InOrder(A);
+	printf("\n");
+	PostOrder(A);
+	printf("\n");
+	printf("TreeLeafSize=%d\n", TreeLeafSize(A));
+	printf("TreeDepth=%d\n", TreeDepth(A));
 	return 0;
 }
