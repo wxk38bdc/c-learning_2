377bcf9099537c428c5ded4504e2b626ef13b69e
wxk38bdc <2227814847@qq.com>
2024-01-19 09:02:42 +0800

    daily submission 2024/1/19

--- WinterVacationHomework/test2.c
+++ WinterVacationHomework/test2.c
@@ -6,3 +6,44 @@
 #include<string.h>
 #include<math.h>
 #include<ctype.h>
+
+void Swap(int* a, int* b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+void bubbleSort(int arr[], int sz)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		int flag = 1;
+		for (j = 0; j < sz - 1 - i; j++)
+		{
+			if (arr[j]>arr[j + 1])
+			{
+				Swap(&arr[j], &arr[j + 1]);
+				flag = 0;
+			}
+		}
+		if (flag == 1)
+		{
+			break;
+		}
+	}
+}
+int main()
+{
+	int arr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	bubbleSort(arr, sz);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	system("pause");
+	return 0;
+}
