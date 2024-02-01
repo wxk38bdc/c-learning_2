f54ee120c0faadc7f07eb37ead870451c0f44b95
wxk38bdc <2227814847@qq.com>
2024-02-01 09:20:40 +0800

    daily submission 2024/2/1

--- Project_2024_1_24/test02.cpp
+++ Project_2024_1_24/test02.cpp
@@ -8,25 +8,60 @@
 #include <Windows.h>
 using namespace std;
 
-class Date
+//class Date
+//{
+//private:
+//	int year;
+//	int month;
+//	int day;
+//public:
+//	Date(int year = 1900, int month = 1, int day = 1)
+//		:year(year), month(month), day(day)
+//	{}//构造函数,初始化列表
+//	void Print()
+//	{
+//		cout << year << "-" << month << "-" << day << endl;
+//	}
+//};
+//int main()
+//{
+//	cout<<"hello world"<<endl;
+//	Date d1;
+//	d1.Print();
+//	return 0;
+//}
+
+void MergeSort(int* arr, int left, int right, int* temp)
 {
-private:
-	int year;
-	int month;
-	int day;
-public:
-	Date(int year = 1900, int month = 1, int day = 1)
-		:year(year), month(month), day(day)
-	{}//构造函数,初始化列表
-	void Print()
+	if (left >= right)
+		return;
+	int mid = (left + right) / 2;
+	MergeSort(arr, left, mid, temp);
+	MergeSort(arr, mid + 1, right, temp);
+	int begin1 = left, end1 = mid;
+	int begin2 = mid + 1, end2 = right;
+	int index = left;
+	while (begin1 <= end1 && begin2 <= end2)
 	{
-		cout << year << "-" << month << "-" << day << endl;
+		if (arr[begin1] < arr[begin2])
+		{
+			temp[index++] = arr[begin1++];
+		}
+		else
+		{
+			temp[index++] = arr[begin2++];
+		}
 	}
-};
-int main()
-{
-	cout<<"hello world"<<endl;
-	Date d1;
-	d1.Print();
-	return 0;
+	while (begin1 <= end1)
+	{
+		temp[index++] = arr[begin1++];
+	}
+	while (begin2 <= end2)
+	{
+		temp[index++] = arr[begin2++];
+	}
+	for (int i = left; i <= right; i++)
+	{
+		arr[i] = temp[i];
+	}
 }
