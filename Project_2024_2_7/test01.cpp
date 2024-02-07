385216d66d07c3d8ae53f68423e7c473f7d60a1d
wxk38bdc <2227814847@qq.com>
2024-02-08 07:46:59 +0800

    daily submission 2024/2/8

---
+++ Project_2024_2_7/test01.cpp
@@ -0,0 +1,123 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+using namespace std;
+//int main()
+//{
+//	int n, q;
+//	cin >> n >> q;
+//	int* a = (int*)malloc(n * sizeof(int));
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		cin >> a[i];
+//	}
+//	int l, r;
+//	while (q--)
+//	{
+//		cin >> l >> r;
+//		--l;
+//		--r;
+//		int ret = 0;
+//		int *b = (int*)calloc(32,sizeof(int));//存储每一位按位或
+//		if (b == nullptr)
+//		{
+//			cout<<"内存分配失败"<<endl;
+//			return 0;
+//		}
+//		for (i = l; i <= r; i++)
+//		{
+//			if ((a[i] >> (i - l)) & 1)
+//			{
+//				b[i - l] = 1;
+//				break;
+//			}
+//		}
+//		for (i = 0; i < 32; i++)
+//		{
+//			if (b[i])
+//			{
+//				ret += 1 << i;
+//			}
+//		}
+//		free(b);
+//		cout << ret << endl;
+//	}
+//	free(a);
+//	return 0;
+//}
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    for (int& x : a) cin >> x;
+
+    // 预处理前缀或
+    vector<int> prefixOr(n);
+    prefixOr[0] = a[0];
+    for (int i = 1; i < n; ++i) {
+        prefixOr[i] = prefixOr[i - 1] | a[i];
+    }
+
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+        --l; --r; // 调整为基于0的索引
+        int result = prefixOr[r];
+        if (l > 0) {
+            result |= prefixOr[l - 1];
+        }
+        cout << result << endl;
+    }
+
+    return 0;
+}
