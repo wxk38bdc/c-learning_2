153e1c3d9b1b48eb813f003fe963bc5dcfa52750
wxk38bdc <2227814847@qq.com>
2024-02-09 09:18:19 +0800

    daily submission 2024/2/9

--- Project_2024_2_7/test01.cpp
+++ Project_2024_2_7/test01.cpp
@@ -2,52 +2,52 @@
 #include <cassert>
 #include <cctype>
 #include <cerrno>
-#include <cfloat>
-#include <ciso646>
-#include <climits>
-#include <clocale>
+//#include <cfloat>
+//#include <ciso646>
+//#include <climits>
+//#include <clocale>
 #include <cmath>
-#include <csetjmp>
-#include <csignal>
-#include <cstdarg>
-#include <cstddef>
+//#include <csetjmp>
+//#include <csignal>
+//#include <cstdarg>
+//#include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
-#include <cwchar>
-#include <cwctype>
+//#include <cwchar>
+//#include <cwctype>
 #include <algorithm>
-#include <bitset>
-#include <complex>
-#include <deque>
-#include <exception>
-#include <fstream>
-#include <functional>
+//#include <bitset>
+//#include <complex>
+//#include <deque>
+//#include <exception>
+//#include <fstream>
+//#include <functional>
 #include <iomanip>
-#include <ios>
-#include <iosfwd>
+//#include <ios>
+//#include <iosfwd>
 #include <iostream>
-#include <istream>
-#include <iterator>
-#include <limits>
-#include <list>
-#include <locale>
-#include <map>
-#include <memory>
-#include <new>
-#include <numeric>
-#include <ostream>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <stdexcept>
-#include <streambuf>
-#include <string>
-#include <typeinfo>
-#include <utility>
-#include <valarray>
+//#include <istream>
+//#include <iterator>
+//#include <limits>
+//#include <list>
+//#include <locale>
+//#include <map>
+//#include <memory>
+//#include <new>
+//#include <numeric>
+//#include <ostream>
+//#include <queue>
+//#include <set>
+//#include <sstream>
+//#include <stack>
+//#include <stdexcept>
+//#include <streambuf>
+//#include <string>
+//#include <typeinfo>
+//#include <utility>
+//#include <valarray>
 #include <vector>
 using namespace std;
 //int main()
@@ -95,29 +95,95 @@ using namespace std;
 //	return 0;
 //}
 
-int main() {
-    int n, q;
-    cin >> n >> q;
-    vector<int> a(n);
-    for (int& x : a) cin >> x;
+//int main() {
+//    int n, q;
+//    cin >> n >> q;
+//    vector<int> a(n);
+//    for (int& x : a) cin >> x;
+//
+//    // 预处理前缀或
+//    vector<int> prefixOr(n);
+//    prefixOr[0] = a[0];
+//    for (int i = 1; i < n; ++i) {
+//        prefixOr[i] = prefixOr[i - 1] | a[i];
+//    }
+//
+//    while (q--) {
+//        int l, r;
+//        cin >> l >> r;
+//        --l; --r; // 调整为基于0的索引
+//        int result = prefixOr[r];
+//        if (l > 0) {
+//            result |= prefixOr[l - 1];
+//        }
+//        cout << result << endl;
+//    }
+//
+//    return 0;
+//}
 
-    // 预处理前缀或
-    vector<int> prefixOr(n);
-    prefixOr[0] = a[0];
-    for (int i = 1; i < n; ++i) {
-        prefixOr[i] = prefixOr[i - 1] | a[i];
-    }
+////3400异或森林
+//bool isSquare(int n)
+//{
+//	int left = 0;
+//	int right = n;
+//	while (left <= right)
+//	{
+//		int mid = left + (right - left) / 2;
+//		if (mid * mid == n)
+//		{
+//			return true;
+//		}
+//		else if (mid * mid < n)
+//		{
+//			left = mid + 1;
+//		}
+//		else
+//		{
+//			right = mid - 1;
+//		}
+//	}
+//	return false;
+//}
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	int* a= new int[n];
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		cin >> a[i];
+//	}
+//	//...
+//	delete[] a;
+//	return 0;
+//}
 
-    while (q--) {
-        int l, r;
-        cin >> l >> r;
-        --l; --r; // 调整为基于0的索引
-        int result = prefixOr[r];
-        if (l > 0) {
-            result |= prefixOr[l - 1];
-        }
-        cout << result << endl;
-    }
+//class Person {
+//private:
+//	int age;
+//	string name;
+//public:
+//	void PrintPersonInfo();
+//};
+//void Person::PrintPersonInfo()
+//{
+//	cout << "name:" << name << " age:" << age << endl;
+//}
+class Date {
+private:
+	int _year;
+	int _month;
+	int _day;
+public:
+	void Display() const
+	{
+		cout << _year << "-" << _month << "-" << _day << endl;
+	}
+};
+int main()
+{
 
-    return 0;
+	return 0;
 }
