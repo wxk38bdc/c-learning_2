e9bf32854aca21cb4b9dd9f6b7c467882a7af811
wxk38bdc <2227814847@qq.com>
2024-02-12 07:30:31 +0800

    daily submission 2024/2/12

--- Project_2024_2_7/test01.cpp
+++ Project_2024_2_7/test01.cpp
@@ -188,26 +188,28 @@ using namespace std;
 //	return 0;
 //}
 
-class A
-{
-public:
-	A() { ++_scount; }
-	A(const A& t) { ++_scount; }
-	~A() { --_scount; }
-	static int GetACount() { return _scount; }
-private:
-	static int _scount;
-};
-int A::_scount = 0;
-void TestA()
-{
-	cout << A::GetACount() << endl;
-	A a1, a2;
-	A a3(a1);
-	cout << A::GetACount() << endl;
-}
-int main()
-{
-	TestA();
-	return 0;
-}
+//class A
+//{
+//public:
+//	A() { ++_scount; }
+//	A(const A& t) { ++_scount; }
+//	~A() { --_scount; }
+//	static int GetACount() { return _scount; }
+//private:
+//	static int _scount;
+//	int num;
+//};
+//int A::_scount = 0;
+//void TestA()
+//{
+//	cout << A::GetACount() << endl;
+//	A a1, a2;
+//	A a3(a1);
+//	cout << A::GetACount() << endl;
+//}
+//int main()
+//{
+//	//TestA();
+//	A a1;
+//	return 0;
+//}
