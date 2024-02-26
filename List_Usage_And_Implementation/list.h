10176d58a045bfbb72ae7dfc42ea616fc26e1b78
wxk38bdc <2227814847@qq.com>
2024-02-27 07:10:49 +0800

    daily submission 2024/2/27

--- List_Usage_And_Implementation/list.h
+++ List_Usage_And_Implementation/list.h
@@ -6,5 +6,237 @@ using namespace std;
 
 namespace myList
 {
+	/*
+	template<class T>
+	class list
+	{
+	private:
+		struct Node
+		{
+			T _data;
+			Node* _next;
+			Node* _prev;
+		};
+		Node* _head;
+		Node* _tail;
+		size_t _size;
+	public:
+		list()
+			:_head(NULL)
+			, _tail(NULL)
+			, _size(0)
+		{}
+		~list()
+		{
+			Node* cur = _head;
+			while (cur)
+			{
+				Node* del = cur;
+				cur = cur->_next;
+				delete del;
+			}
+			_head = _tail = NULL;
+		}
+		void push_back(const T& data)
+		{
+			Node* newNode = new Node;
+			newNode->_data = data;
+			newNode->_next = NULL;
+			newNode->_prev = NULL;
+			if (_head == NULL)
+			{
+				_head = _tail = newNode;
+			}
+			else
+			{
+				_tail->_next = newNode;
+				newNode->_prev = _tail;
+				_tail = newNode;
+			}
+			_size++;
+		}
+		void pop_back()
+		{
+			if (_head == NULL)
+				return;
+			if (_head == _tail)
+			{
+				delete _head;
+				_head = _tail = NULL;
+			}
+			else
+			{
+				Node* del = _tail;
+				_tail = _tail->_prev;
+				_tail->_next = NULL;
+				delete del;
+			}
+			_size--;
+		}
+		void push_front(const T& data)
+		{
+			Node* newNode = new Node;
+			newNode->_data = data;
+			newNode->_next = NULL;
+			newNode->_prev = NULL;
+			if (_head == NULL)
+			{
+				_head = _tail = newNode;
+			}
+			else
+			{
+				newNode->_next = _head;
+				_head->_prev = newNode;
+				_head = newNode;
+			}
+			_size++;
+		}
+		void pop_front()
+		{
+			if (_head == NULL)
+				return;
+			if (_head == _tail)
+			{
+				delete _head;
+				_head = _tail = NULL;
+			}
+			else
+			{
+				Node* del = _head;
+				_head = _head->_next;
+				_head->_prev = NULL;
+				delete del;
+			}
+			_size--;
+		}
+		void insert(size_t pos, const T& data)
+		{
+			if (pos > _size)
+				return;
+			if (pos == 0)
+			{
+				push_front(data);
+			}
+			else if (pos == _size)
+			{
+				push_back(data);
+			}
+			else
+			{
+				Node* cur = _head;
+				while (pos--)
+				{
+					cur = cur->_next;
+				}
+				Node* newNode = new Node;
+				newNode->_data = data;
+				newNode->_next = cur;
+				newNode->_prev = cur->_prev;
+				cur->_prev->_next = newNode;
+				cur->_prev = newNode;
+				_size++;
+			}
+		}
+		void erase(size_t pos)
+		{
+			if (pos >= _size)
+				return;
+			if (pos == 0)
+			{
+				pop_front();
+			}
+			else if (pos == _size - 1)
+			{
+				pop_back();
+			}
+			else
+			{
+				Node* cur = _head;
+				while (pos--)
+				{
+					cur = cur->_next;
+				}
+				cur->_prev->_next = cur->_next;
+				cur->_next->_prev = cur->_prev;
+				delete cur;
+				_size--;
+			}
+		}
+		size_t size()
+		{
+			return _size;
+		}
+		bool empty()
+		{
+			return _size == 0;
+		}
+		void clear()
+		{
+			Node* cur = _head;
+			while (cur)
+			{
+				Node* del = cur;
+				cur = cur->_next;
+				delete del;
+			}
+			_head = _tail = NULL;
+			_size = 0;
+		}
+		void print()
+		{
+			Node* cur = _head;
+			while (cur)
+			{
+				cout << cur->_data << " ";
+				cur = cur->_next;
+			}
+			cout << endl;
+		}
+		*/
+	
+	template<class T>
+	struct __list_node
+	{
+		__list_node<T>* _prev;
+		__list_node<T>* _next;
+		T data;
+	};
+
+	template<class T>
+	class list
+	{
+		typedef __list_node<T> node;
+	public:
+		//带头双向循环链表
+		list()
+		{
+			_head = new node;
+			_head->_prev = _head;
+			_head->_next = _head;
+		}
+		~list()
+		{
+			node* cur = _head->_next;
+			while (cur != _head)
+			{
+				node* del = cur;
+				cur = cur->_next;
+				delete del;
+			}
+			delete _head;
+			_head = NULL;
+		}
+		void push_back(const T& data)
+		{
+			node* newNode = new node;
+			newNode->data = data;
+			newNode->_next = _head;
+			newNode->_prev = _head->_prev;
+			_head->_prev->_next = newNode;
+			_head->_prev = newNode;
+		}
+	private:
+		node* _head;
+	};
 
 }
--- List_Usage_And_Implementation/test_list.cpp
+++ List_Usage_And_Implementation/test_list.cpp
@@ -1,9 +1,73 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include"list.h"
+#include<list>
+void test_list1()
+{
+	list<int> l1;
+	l1.push_back(1);
+	l1.push_back(2);
+	l1.push_back(3);
+	l1.push_back(4);
+	l1.push_back(5);
+	l1.push_back(6);
+	l1.push_back(7);
+	l1.push_back(8);
+	l1.push_back(9);
+	l1.push_back(10);
+	l1.push_front(0);
+	l1.push_front(-1);
+	list<int>::iterator it = l1.begin();
+	while (it != l1.end())
+	{
+		cout << *it << " ";
+		++it;
+	}
+	cout << endl;
+	list<int>::reverse_iterator rit = l1.rbegin();
+	while (rit != l1.rend())
+	{
+		cout << *rit << " ";
+		++rit;
+	}
+	cout << endl;
+}
+void test_list2()
+{
+	//²âÊÔfind,insert,erase
+	list<int> l1;
+	l1.push_back(1);
+	l1.push_back(2);
+	l1.push_back(3);
+	l1.push_back(4);
+	l1.push_back(5);
+	l1.push_back(6);
+	l1.push_back(7);
+	l1.push_back(8);
+	l1.push_back(9);
+	l1.push_back(10);
+	l1.push_front(0);
+	l1.push_front(-1);
 
+	auto pos = find(l1.begin(), l1.end(), 5);
+	if(pos!=l1.end())
+	{
+		l1.insert(pos, 100);
+	}
+	for (auto e : l1)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+	l1.erase(pos);
+	for (auto e : l1)
+	{
+		cout << e << " ";
+	}
+}
 int main()
 {
-
+	//test_list1();
+	//test_list2();
 	return 0;
 }
