5df9f5a42608c731b249244ef968d378fe81dc0d
wxk38bdc <2227814847@qq.com>
2024-03-01 11:53:21 +0800

    daily submission 2024/3/1

--- List_Usage_And_Implementation/list.h
+++ List_Usage_And_Implementation/list.h
@@ -4,271 +4,152 @@
 #include<vector>
 using namespace std;
 
-namespace myList
-{
-	/*
-	template<class T>
-	class list
-	{
-	private:
-		struct Node
-		{
-			T _data;
-			Node* _next;
-			Node* _prev;
-		};
-		Node* _head;
-		Node* _tail;
-		size_t _size;
-	public:
-		list()
-			:_head(NULL)
-			, _tail(NULL)
-			, _size(0)
-		{}
-		~list()
-		{
-			Node* cur = _head;
-			while (cur)
-			{
-				Node* del = cur;
-				cur = cur->_next;
-				delete del;
-			}
-			_head = _tail = NULL;
-		}
-		void push_back(const T& data)
-		{
-			Node* newNode = new Node;
-			newNode->_data = data;
-			newNode->_next = NULL;
-			newNode->_prev = NULL;
-			if (_head == NULL)
-			{
-				_head = _tail = newNode;
-			}
-			else
-			{
-				_tail->_next = newNode;
-				newNode->_prev = _tail;
-				_tail = newNode;
-			}
-			_size++;
-		}
-		void pop_back()
-		{
-			if (_head == NULL)
-				return;
-			if (_head == _tail)
-			{
-				delete _head;
-				_head = _tail = NULL;
-			}
-			else
-			{
-				Node* del = _tail;
-				_tail = _tail->_prev;
-				_tail->_next = NULL;
-				delete del;
-			}
-			_size--;
-		}
-		void push_front(const T& data)
-		{
-			Node* newNode = new Node;
-			newNode->_data = data;
-			newNode->_next = NULL;
-			newNode->_prev = NULL;
-			if (_head == NULL)
-			{
-				_head = _tail = newNode;
-			}
-			else
-			{
-				newNode->_next = _head;
-				_head->_prev = newNode;
-				_head = newNode;
-			}
-			_size++;
-		}
-		void pop_front()
-		{
-			if (_head == NULL)
-				return;
-			if (_head == _tail)
-			{
-				delete _head;
-				_head = _tail = NULL;
-			}
-			else
-			{
-				Node* del = _head;
-				_head = _head->_next;
-				_head->_prev = NULL;
-				delete del;
-			}
-			_size--;
-		}
-		void insert(size_t pos, const T& data)
-		{
-			if (pos > _size)
-				return;
-			if (pos == 0)
-			{
-				push_front(data);
-			}
-			else if (pos == _size)
-			{
-				push_back(data);
-			}
-			else
-			{
-				Node* cur = _head;
-				while (pos--)
-				{
-					cur = cur->_next;
-				}
-				Node* newNode = new Node;
-				newNode->_data = data;
-				newNode->_next = cur;
-				newNode->_prev = cur->_prev;
-				cur->_prev->_next = newNode;
-				cur->_prev = newNode;
-				_size++;
-			}
-		}
-		void erase(size_t pos)
-		{
-			if (pos >= _size)
-				return;
-			if (pos == 0)
-			{
-				pop_front();
-			}
-			else if (pos == _size - 1)
-			{
-				pop_back();
-			}
-			else
-			{
-				Node* cur = _head;
-				while (pos--)
-				{
-					cur = cur->_next;
-				}
-				cur->_prev->_next = cur->_next;
-				cur->_next->_prev = cur->_prev;
-				delete cur;
-				_size--;
-			}
-		}
-		size_t size()
-		{
-			return _size;
-		}
-		bool empty()
-		{
-			return _size == 0;
-		}
-		void clear()
-		{
-			Node* cur = _head;
-			while (cur)
-			{
-				Node* del = cur;
-				cur = cur->_next;
-				delete del;
-			}
-			_head = _tail = NULL;
-			_size = 0;
-		}
-		void print()
-		{
-			Node* cur = _head;
-			while (cur)
-			{
-				cout << cur->_data << " ";
-				cur = cur->_next;
-			}
-			cout << endl;
-		}
-		*/
-	
-	template<class T>
-	struct __list_node
-	{
-		__list_node<T>* _prev;
-		__list_node<T>* _next;
-		T data;
-	};
+namespace myList {
 
-	template<class T>
-	class list
-	{
-		typedef __list_node<T> node;
-	public:
-		//带头双向循环链表
-		list()
-		{
-			_head = new node;
-			_head->_prev = _head;
-			_head->_next = _head;
-		}
-		~list()
-		{
-			node* cur = _head->_next;
-			while (cur != _head)
-			{
-				node* del = cur;
-				cur = cur->_next;
-				delete del;
-			}
-			delete _head;
-			_head = NULL;
-		}
-		void push_back(const T& data)
-		{
-			node* newNode = new node;
-			newNode->data = data;
-			newNode->_next = _head;
-			newNode->_prev = _head->_prev;
-			_head->_prev->_next = newNode;
-			_head->_prev = newNode;
-		}
-		void pop_back()
-		{
-			if (_head->_next == _head)
-				return;
-			node* del = _head->_prev;
-			_head->_prev = del->_prev;
-			del->_prev->_next = _head;
-			delete del;
-		
-		}
-	private:
-		node* _head;
-	};
+    template<class T>
+    struct __list_node {
+        __list_node<T>* _prev;
+        __list_node<T>* _next;
+        T data;
 
-	void test_mylist01()
-	{
-		list<int> l;
-		l.push_back(1);
-		l.push_back(2);
-		l.push_back(3);
-		l.push_back(4);
-		l.push_back(5);
-		l.push_back(6);
-		l.push_back(7);
-		l.push_back(8);
-		l.push_back(9);
-		l.push_back(10);
-		l.push_back(11);
-		l.push_back(12);
-		l.push_back(13);
-		l.push_back(14);
-		l.push_back(15);
+        __list_node(const T& value = T())
+            : _prev(nullptr), _next(nullptr), data(value) {}
+    };
 
-		//l.print();
-	}
+    template<class T, class Ref, class Ptr>
+    struct __list_iterator {
+        typedef __list_node<T> node;
+        typedef __list_iterator<T, Ref, Ptr> self;
+        node* _node;
 
-}
+        // Constructor
+        __list_iterator(node* node = nullptr) : _node(node) {}
+
+        // Dereference operator returns a reference to the data
+        Ref operator*() const { return _node->data; }
+
+        // Arrow operator returns a pointer to the data
+        Ptr operator->() const { return &_node->data; }
+
+        // Prefix increment
+        self& operator++() {
+            _node = _node->_next;
+            return *this;
+        }
+
+        // Postfix increment
+        self operator++(int) {
+            self tmp = *this;
+            _node = _node->_next;
+            return tmp;
+        }
+
+        // Prefix decrement
+        self& operator--() {
+            _node = _node->_prev;
+            return *this;
+        }
+
+        // Postfix decrement
+        self operator--(int) {
+            self tmp = *this;
+            _node = _node->_prev;
+            return tmp;
+        }
+
+        // Equality comparison
+        bool operator ==(const self& other) const {
+            return _node == other._node;
+        }
+
+        // Inequality comparison
+        bool operator !=(const self& other) const {
+            return _node != other._node;
+        }
+    };
+
+    template<class T>
+    class list {
+    public:
+        typedef __list_node<T> node;
+        typedef __list_iterator<T, T&, T*> iterator; // Normal iterator
+        typedef __list_iterator<T, const T&, const T*> const_iterator; // Const iterator
+
+        list() {
+            _head = new node;
+            _head->_prev = _head->_next = _head;
+        }
+
+        ~list() {
+            clear();
+            delete _head;
+        }
+
+        void push_back(const T& data) {
+            node* newNode = new node(data);
+            newNode->_next = _head;
+            newNode->_prev = _head->_prev;
+            _head->_prev->_next = newNode;
+            _head->_prev = newNode;
+        }
+
+        void pop_back() {
+            if (_head->_next == _head) return;
+            node* del = _head->_prev;
+            del->_prev->_next = _head;
+            _head->_prev = del->_prev;
+            delete del;
+        }
+
+        void clear() {
+            node* cur = _head->_next;
+            while (cur != _head) {
+                node* del = cur;
+                cur = cur->_next;
+                delete del;
+            }
+            _head->_next = _head->_prev = _head;
+        }
+
+        iterator begin() {
+            return iterator(_head->_next);
+        }
+
+        iterator end() {
+            return iterator(_head);
+        }
+
+        const_iterator cbegin() const {
+            return const_iterator(_head->_next);
+        }
+
+        const_iterator cend() const {
+            return const_iterator(_head);
+        }
+
+    private:
+        node* _head; // Sentinel node
+    };
+
+    class Date {
+    public:
+        int _year;
+        int _month;
+        int _day;
+
+        Date(int year = 1900, int month = 1, int day = 1)
+            : _year(year), _month(month), _day(day) {}
+    };
+
+    void test_mylist03() {
+        list<Date> l;
+
+        l.push_back(Date(2000, 12, 31));
+        l.push_back(Date(2024, 2, 29));
+
+        // Use the iterator to traverse and print the list
+        for (auto it = l.begin(); it != l.end(); ++it) {
+            cout << it->_year << "-" << it->_month << "-" << it->_day << endl;
+        }
+    }
+} // namespace myList
---
+++ Project_2024_2_28/test02.cpp
@@ -0,0 +1,297 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//int main()
+//{
+//	int row;
+//	int col;
+//	cin >> row;
+//	cin >> col;
+//	int i = 0, j = 0;
+//	int** a = new int*[row];
+//	for (i = 0; i < row; i++)
+//	{
+//		a[i] = new int[col];
+//	}
+//	int** b = new int* [row];
+//	for (i = 0; i < row; i++)
+//	{
+//		b[i] = new int[col];
+//	}
+//	int** c = new int* [row];
+//	for (i = 0; i < row; i++)
+//	{
+//		c[i] = new int[col];
+//	}
+//	for (i = 0; i < row; i++)
+//	{
+//		for (j = 0; j < col; j++)
+//		{
+//			cin >> a[i][j];
+//		}
+//	}
+//	for (i = 0; i < row; i++)
+//	{
+//		for (j = 0; j < col; j++)
+//		{
+//			cin >> b[i][j];
+//		}
+//	}
+//	for (i = 0; i < row; i++)
+//	{
+//		for (j = 0; j < col; j++)
+//		{
+//			c[i][j] = a[i][j] + b[i][j];
+//			cout << c[i][j] << " ";
+//		}
+//		cout << endl;
+//	}
+//	return 0;
+//}
+
+//using namespace std;
+//
+//void stringZip(const char* pInputStr, int lInputLen, char* pOutputStr);
+//
+//int main()
+//{
+//    char a[1000]; // 原始字符串
+//    cin >> a;
+//    int len = strlen(a);
+//    char b[1000]; // 压缩后的字符串
+//    stringZip(a, len, b);
+//    cout << b << endl;
+//    return 0;
+//}
+//
+//void stringZip(const char* pInputStr, int lInputLen, char* pOutputStr) {
+//    string a(pInputStr);
+//    string b;
+//    char prev = '\0';
+//    char cur = a[0];
+//    int count = 0;
+//    for (int i = 0; i < a.size(); i++)
+//    {
+//        if (a[i] == cur)
+//        {
+//            count++;
+//        }
+//        else
+//        {
+//            prev = cur;
+//            cur = a[i];
+//            if (count > 1)
+//            {
+//                char ch = '0' + count;
+//                b += ch;
+//            }
+//            b += prev;
+//            count = 1;
+//        }
+//    }
+//    if (count > 1)
+//    {
+//        char ch = '0' + count;
+//        b += ch;
+//    }
+//    b += cur;
+//    // 将压缩后的字符串拷贝到输出参数指向的内存中
+//    strcpy(pOutputStr, b.c_str());
+//}
+
+//bool isPrime(int i)
+//{
+//	if (i < 2)
+//	{
+//		return false;
+//	}
+//	for (int j = 2; j * j <= i; j++)
+//	{
+//		if (i % j == 0)
+//			return false;
+//	}
+//	return true;
+//}
+//int main()
+//{
+//	list<int>myList;
+//	int i;
+//	while (1)
+//	{
+//		cin >> i;
+//		if (isPrime(i))
+//		{
+//			myList.push_back(i);
+//		}
+//		else
+//		{
+//			break;
+//		}
+//	}
+//	myList.sort();
+//	myList.unique();
+//	for (auto&e : myList)
+//	{
+//		cout << e << " ";
+//	}
+//	return 0;
+//}
+
+
+//// 学生类
+//class Student {
+//public:
+//    int id;
+//    int score;
+//
+//    // 构造函数
+//    Student(int id, int score) : id(id), score(score) {}
+//
+//    // 重载小于运算符，用于排序
+//    bool operator<(const Student& other) const {
+//        return id < other.id; // 按照学号从小到大排序
+//    }
+//};
+//
+//// 找出最高成绩的学生
+//void max(Student* students, int size) {
+//    // 找出最高分
+//    int maxScore = INT_MIN;
+//    for (int i = 0; i < size; ++i) {
+//        maxScore = max(maxScore, students[i].score);
+//    }
+//
+//    // 输出最高分对应的学生
+//    vector<Student> maxStudents; // 存储最高分对应的学生对象
+//    for (int i = 0; i < size; ++i) {
+//        if (students[i].score == maxScore) {
+//            maxStudents.push_back(students[i]);
+//        }
+//    }
+//
+//    // 对最高分对应的学生对象进行排序
+//    sort(maxStudents.begin(), maxStudents.end());
+//
+//    // 输出排序后的最高分对应的学生
+//    for (const auto& student : maxStudents) {
+//        cout << student.id << " " << student.score << endl;
+//    }
+//}
+//
+//int main() {
+//    // 读取输入数据
+//    vector<Student> students;
+//    for (int i = 0; i < 4; ++i) {
+//        int id, score;
+//        cin >> id >> score;
+//        students.push_back(Student(id, score));
+//    }
+//
+//    // 调用函数找出最高成绩的学生
+//    max(&students[0], students.size());
+//
+//    return 0;
+//}
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Matrix类
+class Matrix {
+private:
+    vector<vector<int>> data; // 用于存储矩阵数据
+    int rows; // 矩阵行数
+    int cols; // 矩阵列数
+
+public:
+    // 构造函数
+    Matrix(int rows, int cols) : rows(rows), cols(cols) {
+        // 初始化矩阵数据为0
+        data.resize(rows, vector<int>(cols, 0));
+    }
+
+    // 重载二元运算符+
+    Matrix operator+(const Matrix& other) const {
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                result.data[i][j] = data[i][j] + other.data[i][j];
+            }
+        }
+        return result;
+    }
+
+    // 重载二元运算符-
+    Matrix operator-(const Matrix& other) const {
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                result.data[i][j] = data[i][j] - other.data[i][j];
+            }
+        }
+        return result;
+    }
+
+    // 重载二元运算符*
+    Matrix operator*(const Matrix& other) const {
+        Matrix result(rows, other.cols);
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < other.cols; ++j) {
+                for (int k = 0; k < cols; ++k) {
+                    result.data[i][j] += data[i][k] * other.data[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    // 重载输出运算符<<
+    friend ostream& operator<<(ostream& os, const Matrix& matrix) {
+        for (int i = 0; i < matrix.rows; ++i) {
+            for (int j = 0; j < matrix.cols; ++j) {
+                os << matrix.data[i][j];
+                if (j < matrix.cols - 1) {
+                    os << " ";
+                }
+            }
+            if (i < matrix.rows - 1) {
+                os << endl;
+            }
+        }
+        return os;
+    }
+
+    // 设置矩阵元素
+    void setElement(int row, int col, int value) {
+        if (row >= 0 && row < rows && col >= 0 && col < cols) {
+            data[row][col] = value;
+        }
+    }
+};
+
+int main() {
+    // 读取输入数据
+    Matrix A(3, 3), B(3, 3);
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            int value;
+            cin >> value;
+            A.setElement(i, j, value);
+        }
+    }
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            int value;
+            cin >> value;
+            B.setElement(i, j, value);
+        }
+    }
+
+    // 输出加减乘后的矩阵
+    cout << (A + B) << endl;
+    cout << (A - B) << endl;
+    cout << (A * B) << endl;
+
+    return 0;
+}
