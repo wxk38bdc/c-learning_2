13a55bc788fc4d985d34803a9cb08ab72dd6a0ae
wxk38bdc <2227814847@qq.com>
2024-03-08 12:34:07 +0800

    daily submission 2024/3/8

--- Project_2024_2_28/test3.cpp
+++ Project_2024_2_28/test3.cpp
@@ -1,25 +1,42 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "heap.h"
 #include<ctime>
-int main()
+
+void testHeap1()
 {
-	srand((unsigned int)time(0));
-	//利用堆排序
-	int* arr= new int[100];
-	for (int i = 0; i < 100; i++)
-	{
-		arr[i] = rand()%1000;
-		cout << arr[i] << " ";
-	}
-	cout << endl;
+	//利用堆找到最小的k个数
 	Heap heap;
 	HeapInit(&heap);
-	for (int i = 0; i < 100; i++)
+	int arr[] = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 0 };
+	for (int i = 0; i < 10; i++)
 	{
 		HeapPush(&heap, arr[i]);
 	}
 	HeapPrint(&heap);
+	HeapPop(&heap);
+	HeapPrint(&heap);
+	HeapPop(&heap);
+	HeapPrint(&heap);
+	HeapPop(&heap);
+	HeapPrint(&heap);
+	HeapPop(&heap);
 
+	HeapPush(&heap, 10);
+	HeapPrint(&heap);
+	HeapPush(&heap, 11);
+	HeapPrint(&heap);
+}
+void testHeap2()
+{
+	//利用堆排序
+	Heap heap;
+	HeapInit(&heap);
+	int arr[] = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 0 };
+	for (int i = 0; i < 10; i++)
+	{
+		HeapPush(&heap, arr[i]);
+	}
+	HeapPrint(&heap);
 	while (HeapEmpty(&heap) != true)
 	{
 		cout << HeapTop(&heap) << " ";
@@ -27,6 +44,10 @@ int main()
 	}
 	cout << endl;
 	HeapDestroy(&heap);
-
+}
+int main()
+{
+	testHeap1();
+	//testHeap2();
 	return 0;
 }
