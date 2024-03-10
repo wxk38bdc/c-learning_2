c244474a9dee00c42db1f399602a5f507cf97d3b
wxk38bdc <2227814847@qq.com>
2024-03-10 08:32:58 +0800

    daily submission 2024/3/10

--- Project_2024_3_9/test01.cpp
+++ Project_2024_3_9/test01.cpp
@@ -28,3 +28,44 @@ using namespace std;
 //        return maxFruits;
 //    }
 //};
+
+//class Solution {
+//public:
+//    bool isSame(int* s1, int* s2) {
+//        for (int i = 0; i < 26; i++) {
+//            if (s1[i] != s2[i]) return false;
+//        }
+//        return true;
+//    }
+//
+//    vector<int> findAnagrams(string s, string p) {
+//        if (s.size() < p.size()) return {};
+//        vector<int> ret;
+//        int hash_p[26] = { 0 }, hash_s[26] = { 0 };
+//
+//        // 记录p中的字母计数
+//        for (char e : p) {
+//            hash_p[e - 'a']++;
+//        }
+//
+//        // 初始化第一个窗口
+//        for (int i = 0; i < p.size(); i++) {
+//            hash_s[s[i] - 'a']++;
+//        }
+//        if (isSame(hash_p, hash_s)) {
+//            ret.push_back(0);
+//        }
+//
+//        // 滑动窗口，动态更新计数
+//        for (int i = p.size(); i < s.size(); i++) {
+//            hash_s[s[i] - 'a']++;  // 包含新字符
+//            hash_s[s[i - p.size()] - 'a']--;  // 排除旧字符
+//
+//            if (isSame(hash_p, hash_s)) {
+//                ret.push_back(i - p.size() + 1);
+//            }
+//        }
+//
+//        return ret;
+//    }
+//};
