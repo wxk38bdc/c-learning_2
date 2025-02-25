6a4a8958f6b8ae00f07bf2134b93592198134766
wxk38bdc <2227814847@qq.com>
2025-02-25 12:38:14 +0800

    commit and update RetroSnakeDemo

--- RetroSnakeDemo/gamelogic.h
+++ RetroSnakeDemo/gamelogic.h
@@ -6,16 +6,62 @@
 #include <ctime>
 using namespace std;
 
-const int ROW = 23;
-const int COL = 23;
+const int ROW = 24;
+const int COL = 24;
 const int GRAPH_LENGTH = 700;
 const int GRAPH_WIDTH = 700;
 const int BLOCK_SIZE = 25;
 
+void drawTable(int col, int row);
+void fillBlock(int x, int y, int color = YELLOW);
+
+// 蛇类
+class Snake 
+{
+public:
+	Snake(const vector<vector<int>>& body) : body(body) {}
+	Snake(initializer_list<vector<int>> body) : body(body) {}
+	Snake() {}
+	void addBody(const vector<int>& newBody)
+	{
+		body.push_back(newBody);
+	}
+	void drawSnake(int color = YELLOW)
+	{
+		for (int i = 0; i < body.size(); i++)
+		{
+			fillBlock(body[i][0], body[i][1], color);
+		}
+	}
+	void generateFood(int color = RED)
+	{
+		// 生成随机数
+		int x = rand() % COL;
+		int y = rand() % ROW;
+
+		// 判断食物是否在蛇身上
+		for (int i = 0; i < body.size(); i++)
+		{
+			if (body[i][0] == x && body[i][1] == y)
+			{
+				generateFood();//递归调用
+				return;
+			}
+		}
+
+		fillBlock(x, y, color);
+	}
+    
+private:
+    vector<vector<int>> body;
+};
+
+// 绘制表格
 void drawTable(int col, int row)
 {
-    // 设置线条颜色为黑色
     setlinecolor(BLACK);
+    settextstyle(16, 0, _T("Consolas")); 
+	settextcolor(BLACK);
 
     // 计算表格起始坐标，使其居中
     int startX = (GRAPH_WIDTH - BLOCK_SIZE * row) / 2;
@@ -25,50 +71,42 @@ void drawTable(int col, int row)
     for (int i = 0; i <= col; i++)
     {
         line(startX, startY + i * BLOCK_SIZE, startX + BLOCK_SIZE * row, startY + i * BLOCK_SIZE); // 绘制水平线
+
         // 标出行号
-        //...
+        char rowNum[10];
+        sprintf(rowNum, "%d", i);
+
+        // 将char数组转换为TCHAR类型
+        TCHAR rowNumTCHAR[10];
+        mbstowcs(rowNumTCHAR, rowNum, strlen(rowNum) + 1);  // 转换为宽字符
+
+        // 行号显示在左侧
+        outtextxy(startX - 20, startY + i * BLOCK_SIZE - 8, rowNumTCHAR);
     }
 
     // 绘制列
     for (int i = 0; i <= row; i++)
     {
         line(startX + i * BLOCK_SIZE, startY, startX + i * BLOCK_SIZE, startY + BLOCK_SIZE * col); // 绘制竖线
+
         // 标出列号
-		//...
+        char colNum[10];
+        sprintf(colNum, "%d", i);
+
+        // 将char数组转换为TCHAR类型
+        TCHAR colNumTCHAR[10];
+        mbstowcs(colNumTCHAR, colNum, strlen(colNum) + 1);  // 转换为宽字符
+
+        // 列号显示在上方
+        outtextxy(startX + i * BLOCK_SIZE - 5, startY - 20, colNumTCHAR);
     }
 }
 
-void fillBlock(int x, int y, int color = YELLOW)
+// 填充方块
+void fillBlock(int x, int y, int color)
 {
     int startX = (GRAPH_WIDTH - BLOCK_SIZE * COL) / 2;
     int startY = (GRAPH_LENGTH - BLOCK_SIZE * ROW) / 2;
     setfillcolor(color);
     fillrectangle(startX + x * BLOCK_SIZE, startY + y * BLOCK_SIZE, startX + (x + 1) * BLOCK_SIZE, startY + (y + 1) * BLOCK_SIZE);
-}
-
-void drawSnake(const vector<vector<int>>& snake, int color = YELLOW)
-{
-    for (int i = 0; i < snake.size(); i++)
-    {
-        fillBlock(snake[i][0], snake[i][1], color);
-    }
-}
-
-void generateFood(const vector<vector<int>>& snake)
-{
-    // 生成随机数
-    int x = rand() % COL;
-    int y = rand() % ROW;
-
-    // 判断食物是否在蛇身上
-    for (int i = 0; i < snake.size(); i++)
-    {
-        if (snake[i][0] == x && snake[i][1] == y)
-        {
-			generateFood(snake);//递归调用
-            return;
-        }
-    }
-
-    fillBlock(x, y, RED);
 }
--- RetroSnakeDemo/main.cpp
+++ RetroSnakeDemo/main.cpp
@@ -1,46 +1,44 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include "gamelogic.h"
 
-//蛇的初始位置
-vector<vector<int>> snake = {
-	{14, 11},
-	{13, 11},
-	{13, 10},
-	{12, 10},
-	{11, 10},
-	{11, 9},
-	{11, 8},
-	{11, 7},
-	{10, 7},
-	{9, 7},
-	{8, 7},
-	{7, 7}
-};
-
 
 int main()
 {
 	//设置随机数种子
-    srand(time(nullptr));
+	srand(time(nullptr));
 
-    //初始化图形界面
-    initgraph(GRAPH_LENGTH, GRAPH_WIDTH);
+	//初始化图形界面
+	initgraph(GRAPH_LENGTH, GRAPH_WIDTH);
 
-    //设置背景为白色并刷新
-    setbkcolor(WHITE);
-    cleardevice();
+	//设置背景为白色并刷新
+	setbkcolor(WHITE);
+	cleardevice();
 
-    //绘制表格
-    drawTable(COL, ROW);
+	//绘制表格
+	drawTable(COL, ROW);
+
+	//创建蛇
+	Snake snake({{ 14, 11 },
+		{ 13, 11 },
+		{ 13, 10 },
+		{ 12, 10 },
+		{ 11, 10 },
+		{ 11, 9 },
+		{ 11, 8 },
+		{ 11, 7 },
+		{ 10, 7 },
+		{ 9, 7 },
+		{ 8, 7 },
+		{ 7, 7 }
+	});
 
     //绘制蛇
-    drawSnake(snake, BLUE);
+	snake.drawSnake(GREEN);
 
 	//生成食物
-	generateFood(snake);
+	snake.generateFood(BLUE);
 
-    Sleep(1500);
-    //system("pause");
+    system("pause");
     closegraph();
     return 0;
 }
