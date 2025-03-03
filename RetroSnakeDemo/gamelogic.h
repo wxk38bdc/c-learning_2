21e4b0c8c4c4d210d248053e49d86be7ecdb1442
wxk38bdc <2227814847@qq.com>
2025-03-03 21:51:03 +0800

    modify RetroSnakeDemo

--- RetroSnakeDemo/gamelogic.h
+++ RetroSnakeDemo/gamelogic.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 #include <ctime>
+#include <iostream>
+#include <cassert>
 using namespace std;
 
 const int ROW = 24;
@@ -11,28 +13,68 @@ const int COL = 24;
 const int GRAPH_LENGTH = 700;
 const int GRAPH_WIDTH = 700;
 const int BLOCK_SIZE = 25;
+enum Direction {
+	LEFT = 0,
+	RIGHT = 1,
+	UP = 2,
+	DOWN = 3
+};
 
 void drawTable(int col, int row);
 void fillBlock(int x, int y, int color = YELLOW);
+void drawGameOver();
 
 // 蛇类
-class Snake 
+class Snake
 {
 public:
 	Snake(const vector<vector<int>>& body) : body(body) {}
 	Snake(initializer_list<vector<int>> body) : body(body) {}
 	Snake() {}
+
 	void addBody(const vector<int>& newBody)
 	{
+		if (newBody.size() != 2)
+		{
+			cerr << "The size of newBody must be 2" << endl;
+			assert(false);
+		}
 		body.push_back(newBody);
 	}
-	void drawSnake(int color = YELLOW)
-	{
+
+	void draw(int color = YELLOW)
+	{   //设置背景为白色并刷新
+		setbkcolor(WHITE);
+		cleardevice();
+
+		//绘制表格
+		drawTable(COL, ROW);
+
+		//绘制蛇
 		for (int i = 0; i < body.size(); i++)
 		{
-			fillBlock(body[i][0], body[i][1], color);
+			if (i == 0)
+				fillBlock(body[i][0], body[i][1], RED);
+			else
+				fillBlock(body[i][0], body[i][1], color);
 		}
 	}
+
+	//打印蛇身坐标到控制台
+	void printBodyCoordinate()
+	{
+		//格式美观一些
+		cout << "body: {";
+		for (int i = 0; i < body.size(); i++)
+		{
+			cout << "{" << body[i][0] << ", " << body[i][1] << "}";
+			if (i != body.size() - 1)
+				cout << ", ";
+		}
+		cout << "}" << endl;
+	}
+
+	//生成食物
 	void generateFood(int color = RED)
 	{
 		// 生成随机数
@@ -51,62 +93,151 @@ public:
 
 		fillBlock(x, y, color);
 	}
-    
+
+	//检测键盘输入
+	void checkKeyBoard()
+	{
+		// 检测键盘输入，使用 GetAsyncKeyState 检测按键
+		if (GetAsyncKeyState('W') & 0x8000)  // 如果 W 键按下
+			currentDir = Direction::UP;
+		else if (GetAsyncKeyState('S') & 0x8000)  // 如果 S 键按下
+			currentDir = Direction::DOWN;
+		else if (GetAsyncKeyState('A') & 0x8000)  // 如果 A 键按下
+			currentDir = Direction::LEFT;
+		else if (GetAsyncKeyState('D') & 0x8000)  // 如果 D 键按下
+			currentDir = Direction::RIGHT;
+		else if (GetAsyncKeyState('Q') & 0x8000)  // 如果 Q 键按下，退出游戏
+			gameOver = true;
+	}
+
+	//与checkKeyBoard()为同一功能，只是checkKeyBoard()更加合理
+	void moveSnake() {
+		checkKeyBoard();
+	}
+
+	//自动移动
+	void autoMove()
+	{
+		vector<int> head = body[0];
+		vector<int> newHead = head;
+		//添加新的头部
+		switch (currentDir)
+		{
+		case LEFT:
+			newHead[0]--;
+			break;
+		case RIGHT:
+			newHead[0]++;
+			break;
+		case UP:
+			newHead[1]--;
+			break;
+		case DOWN:
+			newHead[1]++;
+			break;
+		default:
+			break;
+		}
+		//把新的头部插入到头部
+		body.insert(body.begin(), newHead);
+		//删除尾部
+		body.pop_back();
+		//检测是否撞到自己
+		for (int i = 1; i < body.size(); i++)
+		{
+			if (body[i][0] == newHead[0] && body[i][1] == newHead[1])
+			{
+				gameOver = true;
+				break;
+			}
+		}
+		//检测是否撞到墙
+		if (newHead[0] < 0 || newHead[0] >= COL || newHead[1] < 0 || newHead[1] >= ROW)
+		{
+			gameOver = true;
+		}
+		//绘制蛇
+		draw(GREEN);
+	}
+
+	//检测是否游戏结束
+	void checkGameOver()
+	{
+		if (gameOver)
+		{
+			drawGameOver();
+			Sleep(2000);
+			closegraph();
+			exit(0);
+		}
+	}
 private:
-    vector<vector<int>> body;
+	vector<vector<int>> body;
+	bool gameOver = false;
+	Direction currentDir = Direction::RIGHT;
 };
 
 // 绘制表格
 void drawTable(int col, int row)
 {
-    setlinecolor(BLACK);
-    settextstyle(16, 0, _T("Consolas")); 
+	setlinecolor(BLACK);
+	settextstyle(16, 0, _T("Consolas"));
 	settextcolor(BLACK);
 
-    // 计算表格起始坐标，使其居中
-    int startX = (GRAPH_WIDTH - BLOCK_SIZE * row) / 2;
-    int startY = (GRAPH_LENGTH - BLOCK_SIZE * col) / 2;
+	// 计算表格起始坐标，使其居中
+	int startX = (GRAPH_WIDTH - BLOCK_SIZE * row) / 2;
+	int startY = (GRAPH_LENGTH - BLOCK_SIZE * col) / 2;
 
-    // 绘制行
-    for (int i = 0; i <= col; i++)
-    {
-        line(startX, startY + i * BLOCK_SIZE, startX + BLOCK_SIZE * row, startY + i * BLOCK_SIZE); // 绘制水平线
+	// 绘制行
+	for (int i = 0; i <= col; i++)
+	{
+		line(startX, startY + i * BLOCK_SIZE, startX + BLOCK_SIZE * row, startY + i * BLOCK_SIZE); // 绘制水平线
 
-        // 标出行号
-        char rowNum[10];
-        sprintf(rowNum, "%d", i);
+		// 标出行号
+		char rowNum[10];
+		sprintf(rowNum, "%d", i);
 
-        // 将char数组转换为TCHAR类型
-        TCHAR rowNumTCHAR[10];
-        mbstowcs(rowNumTCHAR, rowNum, strlen(rowNum) + 1);  // 转换为宽字符
+		// 将char数组转换为TCHAR类型
+		TCHAR rowNumTCHAR[10];
+		mbstowcs(rowNumTCHAR, rowNum, strlen(rowNum) + 1);  // 转换为宽字符
 
-        // 行号显示在左侧
-        outtextxy(startX - 20, startY + i * BLOCK_SIZE - 8, rowNumTCHAR);
-    }
+		// 行号显示在左侧
+		outtextxy(startX - 20, startY + i * BLOCK_SIZE - 8, rowNumTCHAR);
+	}
 
-    // 绘制列
-    for (int i = 0; i <= row; i++)
-    {
-        line(startX + i * BLOCK_SIZE, startY, startX + i * BLOCK_SIZE, startY + BLOCK_SIZE * col); // 绘制竖线
+	// 绘制列
+	for (int i = 0; i <= row; i++)
+	{
+		line(startX + i * BLOCK_SIZE, startY, startX + i * BLOCK_SIZE, startY + BLOCK_SIZE * col); // 绘制竖线
 
-        // 标出列号
-        char colNum[10];
-        sprintf(colNum, "%d", i);
+		// 标出列号
+		char colNum[10];
+		sprintf(colNum, "%d", i);
 
-        // 将char数组转换为TCHAR类型
-        TCHAR colNumTCHAR[10];
-        mbstowcs(colNumTCHAR, colNum, strlen(colNum) + 1);  // 转换为宽字符
+		// 将char数组转换为TCHAR类型
+		TCHAR colNumTCHAR[10];
+		mbstowcs(colNumTCHAR, colNum, strlen(colNum) + 1);  // 转换为宽字符
 
-        // 列号显示在上方
-        outtextxy(startX + i * BLOCK_SIZE - 5, startY - 20, colNumTCHAR);
-    }
+		// 列号显示在上方
+		outtextxy(startX + i * BLOCK_SIZE - 5, startY - 20, colNumTCHAR);
+	}
 }
 
 // 填充方块
 void fillBlock(int x, int y, int color)
 {
-    int startX = (GRAPH_WIDTH - BLOCK_SIZE * COL) / 2;
-    int startY = (GRAPH_LENGTH - BLOCK_SIZE * ROW) / 2;
-    setfillcolor(color);
-    fillrectangle(startX + x * BLOCK_SIZE, startY + y * BLOCK_SIZE, startX + (x + 1) * BLOCK_SIZE, startY + (y + 1) * BLOCK_SIZE);
+	int startX = (GRAPH_WIDTH - BLOCK_SIZE * COL) / 2;
+	int startY = (GRAPH_LENGTH - BLOCK_SIZE * ROW) / 2;
+	setfillcolor(color);
+	fillrectangle(startX + x * BLOCK_SIZE, startY + y * BLOCK_SIZE, startX + (x + 1) * BLOCK_SIZE, startY + (y + 1) * BLOCK_SIZE);
+}
+
+// 绘制游戏结束画面
+void drawGameOver()
+{
+	setbkcolor(WHITE);
+	cleardevice();
+	settextstyle(64, 0, _T("Consolas"));
+	settextcolor(RED);
+	outtextxy(200, 300, _T("Game Over!"));
 }
--- RetroSnakeDemo/main.cpp
+++ RetroSnakeDemo/main.cpp
@@ -1,7 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include "gamelogic.h"
 
-
 int main()
 {
 	//设置随机数种子
@@ -10,15 +9,8 @@ int main()
 	//初始化图形界面
 	initgraph(GRAPH_LENGTH, GRAPH_WIDTH);
 
-	//设置背景为白色并刷新
-	setbkcolor(WHITE);
-	cleardevice();
-
-	//绘制表格
-	drawTable(COL, ROW);
-
 	//创建蛇
-	Snake snake({{ 14, 11 },
+	Snake snake({ { 14, 11 },
 		{ 13, 11 },
 		{ 13, 10 },
 		{ 12, 10 },
@@ -30,15 +22,35 @@ int main()
 		{ 9, 7 },
 		{ 8, 7 },
 		{ 7, 7 }
-	});
+		});
 
-    //绘制蛇
-	snake.drawSnake(GREEN);
+	//游戏主循环
+	while (true)
+	{
+		//绘制蛇
+		snake.draw(GREEN);
 
-	//生成食物
-	snake.generateFood(BLUE);
+		////生成食物
+		//snake.generateFood(BLUE);
 
-    system("pause");
-    closegraph();
-    return 0;
+		// 检测键盘输入,并改变蛇的移动方向
+		snake.moveSnake();//与checkKeyBoard()为同一功能，只是checkKeyBoard()更加合理
+		//snake.checkKeyBoard();
+
+		//移动蛇
+		snake.autoMove();
+
+		//检测是否游戏结束
+		snake.checkGameOver();
+
+		//打印蛇身坐标到控制台，用于调试
+		snake.printBodyCoordinate();
+
+		//延时，避免游戏过快
+		Sleep(100);
+	}
+
+	system("pause");
+	closegraph();
+	return 0;
 }
